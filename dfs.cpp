d4b6be641f2f75148a127c46da3e3d4a370d8bef
Harsh-bharwani <148901346+Harsh-bharwani@users.noreply.github.com>
2025-03-04 09:34:10 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,7 +1,6 @@
-void dfs(int i, int j , int& mxi, int& mxj, auto& mat){
+void dfs(int i, int j, auto& mat){
         int m=mat.size(), n=mat[0].size();
         if(i<0 || j<0 || i==m || j==n || mat[i][j]==0) return;
-        mxi=max(mxi, i), mxj=max(mxj, j);
         mat[i][j]=0;
         int u[4]={0, 0, -1, 1};
         int v[4]={1, -1, 0, 0};
